c3fa14de24573bd08ee9de0f0392dcb83d27c3d4
Pacifist <desmondmorales92@gmail.com>
2024-01-29 17:56:28 +0200

    logger was created

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-console.log('1')
+require('dotenv').config()
 
-function dal () {}
-dal()
+const config = require("config")
+const logger = require("./utils/logger")('main')
+
+// logger.info("Hello worlds")
+logger.warn("Warn you")
+logger.error('Some error')
--- utils/logger.js
+++ utils/logger.js
@@ -1,6 +1,30 @@
-const {color} = require("colors/safe")
+const colors = require("colors")
+const config = require('config')
+const logLevel = config.logLevel
+
+config.isColorEnabled === '1' ? colors.enable() : colors.disable()
+
 function logger (moduleName) {
-    return {
-        info: (...args) => console.info(color.save(moduleName), args)
+    info = (...args) => console.info(colors.grey(moduleName), ...args)
+    warn = (...args) => console.warn(colors.yellow(moduleName), ...args)
+    error = (...args) => console.error(colors.red(moduleName), ...args)
+    if (logLevel === 'info') {
+        return {
+            info,
+            warn,
+            error
+        }
+    }
+    else if (logLevel === 'warn') {
+        return {
+            warn,
+            error
+        }
+    } else {
+        return {
+            error
+        }
     }
 }
+
+module.exports = logger
