f9adca43d1987797e6351996681fc3653b078280
evhenious <evhenious@gmail.com>
2024-01-29 20:12:39 +0200

    changed logger

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const logger = require('./utils/logger')('main');
+require('dotenv').config()
+const config = require("config")
+
+const logger = require("./utils/logger")('main')
 
 logger.info('the script is running!')
 logger.warn('the script is able to run!');
--- utils/logger.js
+++ utils/logger.js
@@ -1,15 +1,30 @@
-console.log("logger init")
+const colors = require("colors")
+const config = require('config')
+const logLevel = config.logLevel
+
+config.isColorEnabled === '1' ? colors.enable() : colors.disable()
 
 function logger (moduleName) {
-    return {
-        info: (...args) => console.info(moduleName, ...args),
-        warn: (...args) => console.warn(moduleName, ...args),
-        error: (...args) => console.error(moduleName, ...args)
+    info = (...args) => console.info(colors.grey(moduleName), ...args)
+    warn = (...args) => console.warn(colors.yellow(moduleName), ...args)
+    error = (...args) => console.error(colors.red(moduleName), ...args)
+    if (logLevel === 'info') {
+        return {
+            info,
+            warn,
+            error
+        }
     }
-}
-
-function color () {
-    return 'lol'
+    else if (logLevel === 'warn') {
+        return {
+            warn,
+            error
+        }
+    } else {
+        return {
+            error
+        }
+    }
 }
 
 module.exports = logger
