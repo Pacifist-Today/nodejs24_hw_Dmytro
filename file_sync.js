5db38b9797fc52f83e4146d84a1447a56e6e5445
evhenious <evhenious@gmail.com>
2024-02-07 19:31:53 +0200

    helped with code a bit

--- file_sync.js
+++ file_sync.js
@@ -1,51 +1,56 @@
-const fs = require('fs');
-const promise = require('fs/promises');
+const fsAsync = require('fs/promises');
 const path = require('path');
 
 const logger = require('./utils/logger')('file sync');
 
 const start = async () => {
-  const source = await promise.readdir('./source');
-  const target = await promise.readdir('./target');
+  const sourceItems = await fsAsync.readdir('./source');
+  const targetItems = await fsAsync.readdir('./target');
 
-  const transfer = async (catalogue, receiver, universalPath) =>
-    catalogue.map(async (element) => {
+  const transfer = async (origins, targets, universalPath) =>
+    origins.map(async (element) => {
       const sourcePath = path.join('.', 'source', universalPath, element);
       const targetPath = path.join('.', 'target', universalPath, element);
 
-      const isDir = await promise.stat(sourcePath).then((res) => res.isDirectory());
+      const stats = await fsAsync.stat(sourcePath);
 
-      // не работает без then
-      // const isDir = await promise.stat(`${sourcePath}${element}`).isDirectory()
+      // не работает без then - правильно, бо якщо без круглих скобок, то ти викликаєш
+      // метод isDirectory() безпосередньо на промісі, а потрібно на результаті!
+      // const isDir = await (promise.stat(`${sourcePath}${element}`)).isDirectory()
 
       // работает без then
       // const file = await promise.stat(`${sourcePath}${element}`)
       // const isDir = file.isDirectory()
       // console.log(isDir)
 
-      if (isDir) {
-        if (!receiver.includes(element)) {
-          fs.mkdir(targetPath, (err, res) => {
-            logger.info(`Creating directory: ${targetPath}`);
-          });
-        } else logger.warn(`Entering existing directory: ${targetPath}`);
+      if (stats.isFile()) {
+        if (targets.includes(element)) {
+          logger.warn(`${element} is already existed, skipping`);
+          return;
+        }
 
-        const originalDir = await promise.readdir(sourcePath);
-        const receiverDir = await promise.readdir(targetPath);
+        // якщо ми тут, це значить що element - файл, і його немає в target
+        await fsAsync.copyFile(sourcePath, targetPath);
+        logger.info(`Created file: ${targetPath}`);
+        return;
+      }
+
+      if (stats.isDirectory()) {
+        if (!targets.includes(element)) {
+          // неіснуючий каталог створюємо
+          await fsAsync.mkdir(targetPath);
+        }
+
+        const originalDir = await fsAsync.readdir(sourcePath);
+        const receiverDir = await fsAsync.readdir(targetPath);
 
         const newPath = `${universalPath}${element}/`;
 
         await transfer(originalDir, receiverDir, newPath);
-      } else if (!receiver.includes(element) && !isDir) {
-        fs.copyFile(sourcePath, targetPath, (err, res) => {
-          logger.info(`Creating file: ${targetPath}`);
-        });
-      } else if (receiver.includes(element)) {
-        logger.warn(`${element} is already existed`);
       }
     });
 
-  transfer(source, target, '');
+  transfer(sourceItems, targetItems, '');
 };
 
 module.exports = {
