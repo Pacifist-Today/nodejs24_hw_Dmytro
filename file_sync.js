b39763583f14c4fa810599a2a989af498869ffd1
evhenious <evhenious@gmail.com>
2024-02-07 18:50:32 +0200

    format

--- file_sync.js
+++ file_sync.js
@@ -1,55 +1,53 @@
-const fs = require("fs")
-const promise = require("fs/promises")
-const path = require("path")
+const fs = require('fs');
+const promise = require('fs/promises');
+const path = require('path');
 
-const logger = require("./utils/logger")('file sync')
+const logger = require('./utils/logger')('file sync');
 
 const start = async () => {
-    const source = await promise.readdir('./source')
-    const target = await promise.readdir("./target")
+  const source = await promise.readdir('./source');
+  const target = await promise.readdir('./target');
 
-    const transfer = async (catalogue, receiver, universalPath) =>
-        catalogue.map(async element => {
-            const sourcePath = path.join(".", "source", universalPath, element)
-            const targetPath = path.join(".", "target", universalPath, element)
+  const transfer = async (catalogue, receiver, universalPath) =>
+    catalogue.map(async (element) => {
+      const sourcePath = path.join('.', 'source', universalPath, element);
+      const targetPath = path.join('.', 'target', universalPath, element);
 
-            const isDir = await promise.stat(sourcePath).then(res => res.isDirectory())
+      const isDir = await promise.stat(sourcePath).then((res) => res.isDirectory());
 
-            // не работает без then
-            // const isDir = await promise.stat(`${sourcePath}${element}`).isDirectory()
+      // не работает без then
+      // const isDir = await promise.stat(`${sourcePath}${element}`).isDirectory()
 
-            // работает без then
-            // const file = await promise.stat(`${sourcePath}${element}`)
-            // const isDir = file.isDirectory()
-            // console.log(isDir)
+      // работает без then
+      // const file = await promise.stat(`${sourcePath}${element}`)
+      // const isDir = file.isDirectory()
+      // console.log(isDir)
 
-            if (isDir) {
-                if (!receiver.includes(element)) {
-                    fs.mkdir(targetPath, (err, res) => {
-                        logger.info(`Creating directory: ${targetPath}`)
-                    })
-                }
-                else logger.warn(`Entering existing directory: ${targetPath}`)
+      if (isDir) {
+        if (!receiver.includes(element)) {
+          fs.mkdir(targetPath, (err, res) => {
+            logger.info(`Creating directory: ${targetPath}`);
+          });
+        } else logger.warn(`Entering existing directory: ${targetPath}`);
 
-                const originalDir = await promise.readdir(sourcePath)
-                const receiverDir = await promise.readdir(targetPath)
+        const originalDir = await promise.readdir(sourcePath);
+        const receiverDir = await promise.readdir(targetPath);
 
-                const newPath = `${universalPath}${element}/`
+        const newPath = `${universalPath}${element}/`;
 
-                await transfer(originalDir, receiverDir, newPath)
-            }
-            else if (!receiver.includes(element) && !isDir) {
-                fs.copyFile(sourcePath, targetPath, (err, res) => {
-                    logger.info(`Creating file: ${targetPath}`)
-                })
-            }
-            else if (receiver.includes(element)) {
-                logger.warn(`${element} is already existed`)
-            }
-        })
+        await transfer(originalDir, receiverDir, newPath);
+      } else if (!receiver.includes(element) && !isDir) {
+        fs.copyFile(sourcePath, targetPath, (err, res) => {
+          logger.info(`Creating file: ${targetPath}`);
+        });
+      } else if (receiver.includes(element)) {
+        logger.warn(`${element} is already existed`);
+      }
+    });
 
-    transfer(source, target, "")}
+  transfer(source, target, '');
+};
 
 module.exports = {
-    start
-}
+  start,
+};
