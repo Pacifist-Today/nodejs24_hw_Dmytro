8e5874517875862adf33605987ad9b2dec37cc51
evhenious <evhenious@gmail.com>
2024-02-11 22:10:41 +0200

    more readable and simplified logger

--- utils/logger.js
+++ utils/logger.js
@@ -1,6 +1,4 @@
 const fs = require("fs")
-const promise = require("fs/promises")
-const EventEmitter = require("events")
 const path = require("path")
 const colors = require("colors")
 const config = require('config')
@@ -8,72 +6,52 @@ const logLevel = config.logLevel
 
 config.isColorEnabled ? colors.enable() : colors.disable()
 
-const logsPath = path.join(".", "logs")
-const infoPath = path.join(logsPath, "info.log")
-const errorsPath = path.join(logsPath, "errors.log")
+const LOG_FOLDER = 'logs'
 
-const isLogsDir = fs.readdirSync(".").includes(logsPath)
+const infoPath = path.join(LOG_FOLDER, "info.log")
+const errorsPath = path.join(LOG_FOLDER, "errors.log")
 
-!isLogsDir ? fs.mkdirSync(logsPath) : null
+const logsFolderExists = fs.readdirSync(".").includes(LOG_FOLDER)
 
-const writeInfo = fs.createWriteStream(infoPath, {flags: "a"})
-const writeErrors = fs.createWriteStream(errorsPath, {flags: "a"})
+!logsFolderExists && fs.mkdirSync(LOG_FOLDER)
 
-function logger (moduleName) {
+const infoLogStream = fs.createWriteStream(infoPath, {flags: "a"})
+const errorLogStream = fs.createWriteStream(errorsPath, {flags: "a"})
+
+function makeLogger (moduleName) {
     const info = (...args) => {
-        writeLogs("info", ...args)
-        console.info(colors.grey(moduleName), ...args)
+        writeLogsToFileStream(infoLogStream, ...args)
+        if (logLevel === 'info') {
+            console.info(colors.grey(moduleName), ...args)
+        }
     }
+
     const warn = (...args) => {
-        writeLogs("warn", ...args)
-        console.warn(colors.yellow(moduleName), ...args)
+        writeLogsToFileStream(errorLogStream, ...args)
+        if (logLevel !== 'error') {
+            console.warn(colors.yellow(moduleName), ...args)
+        }
     }
+
     const error = (...args) => {
-        writeLogs("error", ...args)
+        writeLogsToFileStream(errorLogStream, ...args)
         console.error(colors.red(moduleName), ...args)
     }
 
-    return loggsCalling(info, warn, error)
+    return {
+        info,
+        warn,
+        error
+    }
 }
 
-function writeLogs(logType, ...args) {
-    if (logType === "info") {
-        writeInfo.write(`${new Date().toLocaleString()}: ${args}\n`)
-    }
-    else if (logType === "error" || logType === "warn") {
-        writeErrors.write(`${new Date().toLocaleString()}: ${args}\n`)
-    }
-    else throw new Error("Incorrect logType")
-}
-
-function loggsCalling(info, warn, error) {
-    if (logLevel === 'info') {
-        return {
-            info,
-            warn,
-            error
-        }
-    }
-    else if (logLevel === 'warn') {
-        return {
-            info: (...args) => writeLogs("info", args),
-            warn,
-            error
-        }
-    }
-    else if (logLevel === 'error') {
-        return {
-            info: (...args) => writeLogs("info", args),
-            warn: (...args) => writeLogs("warn", args),
-            error
-        }
-    }
-    else throw new Error("Incorrect logger level")
+function writeLogsToFileStream(stream, ...args) {
+    stream.write(`${new Date().toLocaleString()}: ${args}\n`)
 }
 
 process.once("beforeExit", () => {
-    writeInfo.end()
-    writeErrors.end()
+    infoLogStream.end()
+    errorLogStream.end()
 })
 
-module.exports = logger
+module.exports = makeLogger
